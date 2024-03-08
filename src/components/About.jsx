86c1cabc235ed8cfa89221d7e5257609a3dcd385
Pkal99 <kaleabay99@gmail.com>
2024-03-08 20:20:49 +0300

    Home updated and About createdu 080324

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-[#436aa5] text-gray-300 '>
+      <div className=' flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+            <div>
+                <p>About Me  43:59</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
