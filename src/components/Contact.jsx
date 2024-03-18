80818003489ed3c673e1cf1af5f534cd459707b4
Pkal99 <kaleabay99@gmail.com>
2024-03-18 16:55:05 +0300

    Afer all components created and completed .jsx completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div name='contact' className='w-full h-screen bg-[#436aa5] flex justify-center p-4'>
-      <form action="https://getform.io/f/jawlxlkb" method='POSt' className='flex flex-col max-w-[600px] w-full'>
-        <div>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Contact</p>
-            <p className='text-gray-300 py-8'>Submit the Form Below or conatct me on Email: Kaleabay99@gmail.com </p>
-        </div>
-        <input className='my-4 p-2 bg-[#ece2e2]' type='text' placeholder='Full Name' name='name'></input>
-        <input className='my-4 p-2 bg-[#ece2e2]' type='email' placeholder='Email address' name='email'></input>
-        <textarea className='bg-[#ece2e2] p-2' rows={10} name='message' placeholder='Message'></textarea>
-        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center' type='submit'>Submit</button>
-        </form>
-      
-    </div>
-  )
-}
+    return (
+      <div name='contact' className='w-full h-screen bg-[#436aa5] flex justify-center items-center p-4'>
+          <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+              <div className='pb-8'>
+                  <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                  <p className='text-gray-300 py-4'>Submit the form below or shoot me an email Kaleabay99@gmail.com</p>
+              </div>
+              <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+              <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+              <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Submit</button>
+          </form>
+      </div>
+    )
+  }
+  
 
 export default Contact
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,10 +7,10 @@ const Project = () => {
   return (
     <div  name="projects" className='w-full md:h-screen bg-[#436aa5] text-gray-300'>
       {/*Container*/}
-      <div className='max-w-[1000px] mx-auto p-4  flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600 '>Projects</p>
-            <p className='py-4'>Check Out some of Projects that I have been working so far.</p>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-4'>
+            <p  className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
+            <p className='py-6'>Check Out some of Projects that I have been working so far.</p>
         </div>
 
         {/*Container*/}
@@ -49,7 +49,6 @@ const Project = () => {
                 </a>
                 <a href='/'> 
                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>           
-                
                 </a>
 
               
